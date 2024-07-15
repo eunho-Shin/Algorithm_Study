8e51193452683ea5cabeeb5e79d858b14b13c5a6
SONG JU HEE <80883709+songzuhee@users.noreply.github.com>
2024-07-15 21:28:25 +0900

    최대공약수와 최소공배수 / 송주희

---
+++ 0715_최대공약수와 최소공배수/송주희.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        int m = sc.nextInt();
+
+        System.out.println(gcd(n, m));
+        System.out.println(lcm(n, m));
+    }
+
+    public static int gcd(int n, int m) {
+        while (m > 0) {
+            int temp = m;
+            m = n % m;
+            n = temp;
+        }
+        return n;
+    }
+
+    public static int lcm(int n, int m) {
+        return (n * m) / gcd(n, m);
+    }
+}
